ccd56fa477cc2bd2e05b26087c0803af04da92f0
Ikhsan Azri <ikhsanazri99@gmail.com>
2022-06-12 23:07:47 +0700

    Selesai juga hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs  = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,34 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack)=>{
+  let arrResult=[];
+  fs.readFile(file1,"utf-8",(err, data1)=> {
+      if(err) {
+        return fnCallBack(err, null);
+      }
+      fs.readFile(file2,"utf-8",(err, data2)=> {
+        if(err) {
+          return fnCallBack(err, null);
+        }
+        fs.readFile(file3,"utf-8",(err, data3)=> {
+          if(err) {
+            return fnCallBack(err, null);
+          }
+          let dtFile1 = JSON.parse(data1);
+          let dtFile2 = JSON.parse(data2);
+          let dtFile3 = JSON.parse(data3);
+          arrResult.push(dtFile1.message.split(" ")[1]);
+          arrResult.push(dtFile2[0].message.split(" ")[1]);
+          arrResult.push(dtFile3[0].data.message.split(" ")[1]);
+          
+        });
+      });
+    });
+
+    
+  
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
